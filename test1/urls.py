f43fb8519345114841518825f730bdfc1dc270c2
eyeene <euealfeseha@gmail.com>
2022-08-16 14:22:16 +0300

    dd

--- test1/urls.py
+++ test1/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('a', views.a),
-    path('', views.av.as_view()),
+    path('', views.a),
+    path('a', views.av.as_view()),
     path('b',views.b),
 ]
